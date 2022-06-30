611a1835bf15e63f5920db6846660e8d9847fcdf
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-01 09:17:33 +1000

    Improve test

--- test_new_works_1.cpp
+++ test_new_works_1.cpp
@@ -31,8 +31,19 @@ int main() {
         assert(grid->side_length == sl);
         vector<size_t> ndiv_correct = {2, 2};
         assert(equal_vectors(grid->ndiv, ndiv_correct));
-        vector<double> xmin_correct = {-0.1, -0.1};
+        vector<double> xmin_correct = {-0.01, -0.01};
         assert(equal_vectors(grid->xmin, xmin_correct));
+        vector<double> xmax_correct = {-0.01 + sl * 2.0, -0.01 + sl * 2.0};
+        assert(equal_vectors(grid->xmax, xmax_correct));
+        assert(grid->bounding_boxes.size() == 2);
+        auto bbox_0 = grid->bounding_boxes[0];
+        vector<vector<double>> x_min_max_correct = {{0.0, 1.0}, {0.0, 1.0}};
+        assert(equal_vectors(bbox_0[0], x_min_max_correct[0]));
+        assert(equal_vectors(bbox_0[1], x_min_max_correct[1]));
+        auto bbox_1 = grid->bounding_boxes[1];
+        assert(equal_vectors(bbox_1[0], x_min_max_correct[0]));
+        assert(equal_vectors(bbox_1[1], x_min_max_correct[1]));
+        assert(grid->containers.size() == 4);
 
     } catch (char const *msg) {
         cout << "ERROR: " << msg << endl;
