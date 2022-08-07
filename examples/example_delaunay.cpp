83c7b14978d299c5727d51bdb02a53a1d88622d7
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-08-07 11:10:16 +1000

    Impl Delaunay

--- examples/example_delaunay.cpp
+++ examples/example_delaunay.cpp
@@ -7,21 +7,36 @@ using namespace std;
 
 int main() {
     try {
-        // data
-        vector<vector<double>> coordinates = {
-            {0.0, 0.0, 0.0},  // last column is the temperature
-            {0.5, 0.85, 0.986154146165801},
-            {1.0, 0.0, 1.0},
-            {1.0, 1.7, 1.972308292331602},
-            {1.5, 0.85, 1.724093964956667},
-            {2.0, 0.0, 2.0},
-            {2.0, 1.7, 2.6248809496813372},
-            {2.5, 0.85, 2.640549185302179},
-            {3.0, 1.7, 3.448187929913334},
+        // "cloud" of points
+        vector<vector<double>> cloud = {
+            {0.478554, 0.00869692},
+            {0.13928, 0.180603},
+            {0.578587, 0.760349},
+            {0.903726, 0.975904},
+            {0.0980015, 0.981755},
+            {0.133721, 0.348832},
+            {0.648071, 0.369534},
+            {0.230951, 0.558482},
+            {0.0307942, 0.459123},
+            {0.540745, 0.331184},
         };
 
-        // allocate Delaunay structure
-        auto del = Delaunay::make_new(coordinates);
+        // generate Delaunay triangulation
+        auto del = Delaunay::make_new(cloud, false);
+
+        // print the point coordinates
+        auto npoint = del->coordinates.size();
+        cout << "npoint = " << npoint << endl;
+        for (size_t p = 0; p < npoint; p++) {
+            cout << "x, y, T = " << del->coordinates[p][0] << ", " << del->coordinates[p][1] << ", " << del->coordinates[p][2] << endl;
+        }
+
+        // print the triangle connectivities
+        auto ntriangle = del->triangles.size();
+        cout << "ntriangle = " << ntriangle << endl;
+        for (size_t t = 0; t < ntriangle; t++) {
+            cout << "v0, v1, v2 = " << del->triangles[t][0] << ", " << del->triangles[t][1] << ", " << del->triangles[t][2] << endl;
+        }
 
     } catch (char const *msg) {
         cout << "ERROR: " << msg << endl;
--- src/delaunay.h
+++ src/delaunay.h
@@ -9,26 +9,30 @@
 #include <vector>
 
 extern "C" {
+#include "triangle/constants.h"
 #include "triangle/interface_triangle.h"
 }
 
 using namespace std;
 
+// Implements the Delaunay triangulation
 struct Delaunay {
-    ExtTriangle* ext_triangle;  // data allocated by the c-code
+    // list of coordinates such as `[[x0,y0,T0], [x1,y1,T1], [x2,y2,T2], [x3,y3,T3]]`
+    // where `T[i]` are the values (e.g., temperatures) at that coordinate and will be used for interpolation
+    vector<vector<double>> coordinates;
 
-    // destructor to make sure we clean Triangle data
-    ~Delaunay() {
-        drop_triangle(this->ext_triangle);
-    }
+    // list of connectivity (topology) such as `[[0,2,1], [2,1,0]]`
+    vector<vector<size_t>> triangles;
 
-    static std::unique_ptr<Delaunay> make_new(vector<vector<double>> const& coordinates) {
+    // Returns a new Delaunay triangulation from a "cloud" of points
+    // `cloud` is a list of coordinates such as `[[x0,y0,...], [x1,y1,..], ...]`
+    static std::unique_ptr<Delaunay> make_new(vector<vector<double>> const& cloud, bool verbose) {
         // constants
-        size_t npoint = coordinates.size();
+        size_t npoint = cloud.size();
         if (npoint < 3) {
             throw "number of points must be >= 3";
         }
-        size_t ncol = coordinates[0].size();
+        size_t ncol = cloud[0].size();
         if (ncol < 2) {
             throw "coordinates.ncol must be >= 2";
         }
@@ -39,10 +43,64 @@ struct Delaunay {
             throw "cannot allocate triangle data";
         }
 
+        // set points
+        for (size_t index = 0; index < npoint; index++) {
+            int status = set_point(ext_triangle, index, cloud[index][0], cloud[index][1]);
+            if (status != TRITET_SUCCESS) {
+                if (status == TRITET_ERROR_NULL_DATA) {
+                    throw "INTERNAL ERROR: found NULL data";
+                }
+                if (status == TRITET_ERROR_NULL_POINT_LIST) {
+                    throw "INTERNAL ERROR: found NULL point list";
+                }
+                if (status == TRITET_ERROR_INVALID_POINT_INDEX) {
+                    throw "index of point is out of bounds";
+                }
+                throw "INTERNAL ERROR: some error occurred";
+            }
+        }
+
+        // perform Delaunay triangulation
+        int status = run_delaunay(ext_triangle, verbose);
+        if (status != TRITET_SUCCESS) {
+            if (status == TRITET_ERROR_NULL_DATA) {
+                throw "INTERNAL ERROR: found NULL data";
+            }
+            if (status == TRITET_ERROR_NULL_POINT_LIST) {
+                throw "INTERNAL ERROR: found NULL point list";
+            }
+            throw "INTERNAL ERROR: some error occurred";
+        }
+
+        // extract points from Triangle' data structures
+        size_t n_mesh_point = get_npoint(ext_triangle);
+        auto coordinates = vector<vector<double>>(n_mesh_point);
+        for (size_t point_index = 0; point_index < n_mesh_point; point_index++) {
+            coordinates[point_index] = vector<double>(3);  // x,y,T (in case we want to use the third value for interpolations)
+            coordinates[point_index][0] = get_point(ext_triangle, point_index, 0);
+            coordinates[point_index][1] = get_point(ext_triangle, point_index, 1);
+            coordinates[point_index][2] = 0.0;  // zero "temperature"
+        }
+
+        // extract triangles from Triangle' data structures
+        size_t ntriangle = get_ntriangle(ext_triangle);
+        size_t ncorner = get_ncorner(ext_triangle);  // this should be 3 because we didn't require "o2" triangles
+        auto triangles = vector<vector<size_t>>(ntriangle);
+        for (size_t tri_index = 0; tri_index < ntriangle; tri_index++) {
+            triangles[tri_index] = vector<size_t>(ncorner);
+            for (size_t corner = 0; corner < ncorner; corner++) {
+                triangles[tri_index][corner] = get_triangle_corner(ext_triangle, tri_index, corner);
+            }
+        }
+
+        // make sure we clean Triangle data
+        drop_triangle(ext_triangle);
+
         // allocate Delaunay structure
         return std::unique_ptr<Delaunay>{
             new Delaunay{
-                ext_triangle,
+                coordinates,
+                triangles,
             }};
     }
 };
